8c6395ec2db52cd9f62b18288c006926eac5ef1d
Luis Barufi <luisbarufi@gmail.com>
2024-02-13 23:21:54 -0300

    Add SystemRequirement CRUD

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -3,7 +3,7 @@ class SystemRequirement < ApplicationRecord
   include Paginatable
 
   has_many :games, dependent: :restrict_with_error
-  
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :operational_system, presence: true
   validates :storage, presence: true

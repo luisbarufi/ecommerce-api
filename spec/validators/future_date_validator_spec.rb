92effc08c0cd869c68bb3c1c68c56e477b3b1947
Luis Barufi <luisbarufi@gmail.com>
2024-02-18 21:01:31 -0300

    Fixes validator test

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -11,11 +11,11 @@ describe FutureDateValidator do
 
   context "when date is before current date" do
     before { subject.date = 1.day.ago }
-  
+
     it "should be invalid" do
       expect(subject.valid?).to be_falsey
     end
-    
+
     it "adds an error on model" do
       subject.valid?
       expect(subject.errors.keys).to include(:date)
